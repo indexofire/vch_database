049b06eb6b49266cea1c3a57988fda9f9282ce5d
indexofire <indexofire@gmail.com>
2014-10-31 07:09:19 +0800

    add data

---
+++ mlst_pipeline.py
@@ -0,0 +1,8 @@
+import subprocess
+
+with open('mlst.txt', 'r') as f:
+    accs = f.read().split("\n")
+
+for acc in accs:
+    subprocess.call('fastq-dump --split-files ../sra/%s.sra' % acc, shell=True)
+    subprocess.call('srst2 --input_se %s_1.fastq %s_2.fastq --log --mlst_delimiter _ --mlst_db Vibrio_cholerae.fasta --mlst_definitions vcholerae.txt --output %s' % (acc, acc, acc), shell=True)
