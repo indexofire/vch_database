bbcb6364d1452b14417d9d516eb03aa9cd4605ad
indexofire <indexofire@gmail.com>
2014-11-01 07:07:01 +0800

    fix mlst pipeline error

--- script/mlst_pipeline.py
+++ script/mlst_pipeline.py
@@ -5,4 +5,4 @@ with open('mlst.txt', 'r') as f:
 
 for acc in accs:
     subprocess.call('fastq-dump --split-files ../sra/%s.sra' % acc, shell=True)
-    subprocess.call('srst2 --input_se %s_1.fastq %s_2.fastq --log --mlst_delimiter _ --mlst_db Vibrio_cholerae.fasta --mlst_definitions vcholerae.txt --output %s' % (acc, acc, acc), shell=True)
+    subprocess.call('srst2 --input_pe %s_1.fastq %s_2.fastq --log --mlst_delimiter _ --mlst_db Vibrio_cholerae.fasta --mlst_definitions vcholerae.txt --output %s' % (acc, acc, acc), shell=True)
